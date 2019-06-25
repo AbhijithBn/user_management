5371a0ac51903df207ac1aac9cb2ef85d6730e15
Abhijith <b.n.abhijith@gmail.com>
2019-06-25 19:21:33 +0530

    UI

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://localhost/loginform'
+	// 'url' : 'mongodb://localhost/loginform'
+	'url':'mongodb+srv://AbhijithBn:Abhi123@cluster0-q3zcw.mongodb.net/test?retryWrites=true&w=majority'
+
 }
