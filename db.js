f43d503aff581158f339e41dccda3c670f76cf18
Abhijith B N <b.n.abhijith@gmail.com>
2019-06-26 14:31:47 +0530

    Delete db.js

--- db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'url':'mongodb+srv://AbhijithBn:Abhi123@cluster0-q3zcw.mongodb.net/test?retryWrites=true&w=majority'
-}
-// 'url' : 'mongodb://localhost/loginform'
-// in the above URL the username is the username of the database user and not the username of the account
-// and the password is the password for the database admin and not the password for the account
